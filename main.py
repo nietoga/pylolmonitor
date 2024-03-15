c24664ce3b32c74b69f1f8a5c06cdc7aa6ee8acf
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-15 14:20:08 -0500

    Add default mail sender config

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from mail import send_mail
 from supervisor import is_lol_runing
+import config
+
+DEFAULT_MAIL_SENDER = config.get("DEFAULT_MAIL_SENDER")
 
 if __name__ == "__main__":
     if is_lol_runing():
         send_mail(
-            "agusnieto.gg@gmail.com",
+            DEFAULT_MAIL_SENDER,
             "agusngarci@gmail.com",
             "LoL Warning",
             "This motherfucker is trying to install LoL",
