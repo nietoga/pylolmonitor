7ec5f29d069cf440ccaeac614aeb6a452f2e8812
Agustin Nieto <agusnieto.gg@gmail.com>
2024-12-03 21:56:51 -0500

    add default keys in case missing

--- external_services/mail.py
+++ external_services/mail.py
@@ -1,8 +1,8 @@
 import config
 from mail.mailjet import MailJetService
 
-MAILJET_API_KEY = config.get("MAILJET_API_KEY")
-MAILJET_API_SECRET = config.get("MAILJET_API_SECRET")
+MAILJET_API_KEY = config.get("MAILJET_API_KEY") or ""
+MAILJET_API_SECRET = config.get("MAILJET_API_SECRET") or ""
 
 mail_service = MailJetService(
     api_key=MAILJET_API_KEY,
